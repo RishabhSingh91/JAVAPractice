222ac76ec88fb9603e0e641f9c8198c08ca26127
jamwa <jamwa@LAPTOP-AOT80K1O>
2021-01-05 23:16:49 +0530

    Sorting function

---
+++ Practice/src/HomePractice/SortingPrac.java
@@ -0,0 +1,32 @@
+package HomePractice;
+
+
+public class SortingPrac {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[] {12,17,5,6,3,1,9,0};
+		int temp=0;
+		
+		for(int i =0; i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+					
+				}
+			}
+		}
+		for(int k:arr)
+		{
+			System.out.println(k);
+		}
+		
+
+	}
+
+}
